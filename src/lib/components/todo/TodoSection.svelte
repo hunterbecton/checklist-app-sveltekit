6350582091034e5d71ead0181ae6c1d61ade7681
Hunter Garrett <hunter@hunterbecton.com>
2023-01-30 14:04:21 -0500

    Added localstorage

--- src/lib/components/todo/TodoSection.svelte
+++ src/lib/components/todo/TodoSection.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <section class="mt-8 space-y-4">
-	{#each $todos as todo (todo.id)}
+	{#each $todos as todo}
 		<TodoInput {todo} />
 	{/each}
 </section>
--- src/lib/store/TodoStore.js
+++ src/lib/store/TodoStore.js
@@ -1,7 +1,16 @@
-import { writable } from 'svelte/store';
 import { v4 as uuidv4 } from 'uuid';
+import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
 
-export const todos = writable([]);
+const data = browser ? JSON.parse(window.localStorage.getItem('st-todo-list')) ?? [] : [];
+
+export const todos = writable(data);
+
+todos.subscribe((value) => {
+	if (browser) {
+		localStorage.setItem('st-todo-list', JSON.stringify(value));
+	}
+});
 
 export const addTodo = () => {
 	todos.update((currentTodos) => {
